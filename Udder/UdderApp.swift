2c681936659b0de13bddaf1d6f9dc9634514bb23
jeongmu <jeongmuya@gmail.com>
2025-12-30 18:52:03 +0900

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by 정무 on 12/30/25.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
